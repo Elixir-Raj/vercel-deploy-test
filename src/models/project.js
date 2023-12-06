4eff2a4528f9fadf7f0232d2dc8f312c64a00a82
Amrit-Raj12 <amrit.raj1224@gmail.com>
2023-12-06 08:15:10 +0530

    First

---
+++ src/models/project.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const projectSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    thumbnail: {
+        type: Buffer
+    },
+    description: [{
+        type: String,
+        required: true,
+        trim: true
+    }],
+    date: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    link: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    imgSrc: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+})
+
+const Project = mongoose.model('Project', projectSchema)
+
+module.exports = Project
